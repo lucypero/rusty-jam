9b286f9d447a75910b388fe495d5d657abe5bcaa
Lucy <lucyperopero@gmail.com>
2021-08-21 23:26:29 -0300

    init

---
+++ src/main.rs
@@ -0,0 +1,48 @@
+use bevy::prelude::*;
+
+fn main() {
+    App::build()
+        .add_plugins(DefaultPlugins)
+        .add_plugin(HelloPlugin)
+        .run();
+}
+
+pub struct HelloPlugin;
+
+impl Plugin for HelloPlugin {
+    fn build(&self, app: &mut AppBuilder) {
+        app
+            .insert_resource(GreetTimer(Timer::from_seconds(2.0, true)))
+            .add_startup_system(add_people.system())
+            .add_system(greet_people.system());
+    }
+}
+
+struct Person;
+
+struct Name(String);
+
+struct GreetTimer(Timer);
+
+fn add_people(mut commands: Commands) {
+    commands
+        .spawn()
+        .insert(Person)
+        .insert(Name("Elaina Proctor".to_string()));
+    commands
+        .spawn()
+        .insert(Person)
+        .insert(Name("Renzo Hume".to_string()));
+    commands
+        .spawn()
+        .insert(Person)
+        .insert(Name("Zayna Nieves".to_string()));
+}
+
+fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
+    if timer.0.tick(time.delta()).just_finished() {
+        for name in query.iter() {
+            println!("hello {}", name.0)
+        }
+    }
+}

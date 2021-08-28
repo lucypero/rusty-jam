6af22b7a95c4016d6e7809596e873f5eac558c9f
Lucas Kent <rubickent@gmail.com>
2021-08-28 23:23:33 +1000

    shopkeeper will choose between blast or spawn minion attacks

--- src/shopkeeper.rs
+++ src/shopkeeper.rs
@@ -1,7 +1,8 @@
 use bevy::prelude::*;
 use crate::player::Player;
-use crate::collision::{Hurtbox, Team};
+use crate::collision::{Hurtbox, Team, HitBoxEvent, CanHitTeam};
 use crate::skeleton::SkeletonBundle;
+use rand::seq::SliceRandom;
 
 #[derive(Bundle)]
 pub struct ShopkeeperBundle {
@@ -16,6 +17,7 @@ impl ShopkeeperBundle {
             shopkeeper: Shopkeeper {
                 action: ShopkeeperAction::Idle,
                 frame: 0,
+                frames_since_last_ability: 0,
             },
             hurtbox: Hurtbox {
                 size: Vec2::new(30.0, 50.0),
@@ -35,16 +37,19 @@ impl ShopkeeperBundle {
     }
 }
 
+#[derive(Clone)]
 pub enum ShopkeeperAction {
     Idle,
     Walk,
-    Damaged,
     SpawnMinions,
+    Blast,
+    Damaged,
 }
 
 pub struct Shopkeeper {
     action: ShopkeeperAction,
     frame: u64,
+    frames_since_last_ability: u64,
 }
 
 impl Shopkeeper {
@@ -59,6 +64,7 @@ pub fn shopkeeper_system(
     mut materials: ResMut<Assets<ColorMaterial>>,
     mut player_query: Query<(&mut Player, &Transform)>,
     mut shopkeeper_query: Query<(&mut Shopkeeper, &mut Hurtbox, &mut Transform), Without<Player>>,
+    mut hitbox: EventWriter<HitBoxEvent>,
 ) {
     if let Ok((_player, player_transform)) = player_query.single_mut() {
         for (mut shopkeeper, mut hurtbox, transform) in shopkeeper_query.iter_mut() {
@@ -72,12 +78,36 @@ pub fn shopkeeper_system(
                 ShopkeeperAction::Idle => {
                     if difference.length() < 100.0 {
                         shopkeeper.set_action(ShopkeeperAction::Walk);
+                        shopkeeper.frames_since_last_ability = 0;
                     }
                 }
                 ShopkeeperAction::Walk => {
                     hurtbox.vel = difference.truncate().normalize() * 1.5;
-                    if shopkeeper.frame > 300 {
-                        shopkeeper.set_action(ShopkeeperAction::SpawnMinions);
+                    if shopkeeper.frames_since_last_ability > 300 {
+                        let action = [ShopkeeperAction::SpawnMinions, ShopkeeperAction::Blast].choose(&mut rand::thread_rng()).unwrap().clone();
+                        shopkeeper.set_action(action);
+                    }
+                }
+                ShopkeeperAction::Blast => {
+                    hurtbox.invincible = true;
+                    let angle = difference.angle_between(Vec3::new(1.0, 0.0, 0.0)); // TODO: angle_between docs say Vec3(0, 0, 0) is bad...?
+                    if shopkeeper.frame < 10 {
+                        hurtbox.vel = Vec2::new(angle.cos(), angle.sin()) * -20.0;
+                    }
+
+                    if shopkeeper.frame > 55 && shopkeeper.frame < 100 {
+                        hitbox.send(HitBoxEvent {
+                            position: transform.translation.truncate() + Vec2::new(angle.cos(), angle.sin()) * 150.0,
+                            size: Vec2::new(300.0, 300.0),
+                            damage: 5,
+                            knockback: 70.0,
+                            can_hit: CanHitTeam::Player,
+                        });
+                    }
+                    if shopkeeper.frame > 105 {
+                        hurtbox.invincible = false;
+                        shopkeeper.frames_since_last_ability = 0;
+                        shopkeeper.set_action(ShopkeeperAction::Walk);
                     }
                 }
                 // spawn minions perpindular to the player
@@ -86,9 +116,9 @@ pub fn shopkeeper_system(
                     let angle = difference.angle_between(Vec3::new(1.0, 0.0, 0.0)); // TODO: angle_between docs say Vec3(0, 0, 0) is bad...?
                     let perpindicular = angle + std::f32::consts::FRAC_PI_2;
                     if shopkeeper.frame % 2 == 0 {
-                        hurtbox.vel += Vec2::new(perpindicular.cos(), perpindicular.sin()) * 20.0;
+                        hurtbox.vel = Vec2::new(perpindicular.cos(), perpindicular.sin()) * 20.0;
                     } else {
-                        hurtbox.vel -= Vec2::new(perpindicular.cos(), perpindicular.sin()) * 20.0;
+                        hurtbox.vel = Vec2::new(perpindicular.cos(), perpindicular.sin()) * -20.0;
                     }
 
                     if shopkeeper.frame == 50 {
@@ -101,6 +131,7 @@ pub fn shopkeeper_system(
                     }
                     if shopkeeper.frame > 60 {
                         hurtbox.invincible = false;
+                        shopkeeper.frames_since_last_ability = 0;
                         shopkeeper.set_action(ShopkeeperAction::Walk);
                     }
                 }
@@ -113,6 +144,7 @@ pub fn shopkeeper_system(
             }
 
             shopkeeper.frame += 1;
+            shopkeeper.frames_since_last_ability += 1;
         }
     }
 }
